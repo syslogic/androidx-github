ff50e346e6adefff0157b6c6469ba3b57905b930
Martin Zeitler <syslogic23@gmail.com>
2018-11-02 14:52:57 +0100

    useless GITHUB_WEB_BASE_URL removed.

--- mobile/src/main/java/io/syslogic/githubtrends/constants/Constants.java
+++ mobile/src/main/java/io/syslogic/githubtrends/constants/Constants.java
@@ -4,8 +4,6 @@ public final class Constants {
 
     public static final String GITHUB_API_BASE_URL    = "https://api.github.com/";
 
-    public static final String GITHUB_WEB_BASE_URL    = "https://github.com/";
-
     public static final String ARGUMENT_ITEM_ID       = "itemId";
 
 }
