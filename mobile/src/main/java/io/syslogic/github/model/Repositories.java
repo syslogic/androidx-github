efc228ac321d0a6da5cc1580fd524ce4514e95e1
Martin Zeitler <syslogic23@gmail.com>
2018-11-04 15:15:16 +0100

    added a @Bindable annotation.

--- mobile/src/main/java/io/syslogic/github/model/Repositories.java
+++ mobile/src/main/java/io/syslogic/github/model/Repositories.java
@@ -4,7 +4,10 @@ import com.google.gson.annotations.SerializedName;
 
 import java.util.ArrayList;
 
-public class Repositories {
+import androidx.databinding.Bindable;
+import androidx.databinding.Observable;
+
+public class Repositories implements Observable {
 
     @SerializedName("items")
     private ArrayList<Repository> mItems;
@@ -12,19 +15,30 @@ public class Repositories {
     @SerializedName("total_count")
     private long count;
 
-    public ArrayList<Repository> getRepositories() {
-        return this.mItems;
-    }
-
-    public long getCount() {
-        return this.count;
-    }
-
     public void setRepositories(ArrayList<Repository> items) {
         this.mItems = items;
     }
 
     public void setCount(long value) {
         this.count = value;
+    }
+
+    public ArrayList<Repository> getRepositories() {
+        return this.mItems;
+    }
+
+    @Bindable
+    public long getCount() {
+        return this.count;
+    }
+
+    @Override
+    public void addOnPropertyChangedCallback(OnPropertyChangedCallback callback) {
+
+    }
+
+    @Override
+    public void removeOnPropertyChangedCallback(OnPropertyChangedCallback callback) {
+
     }
 }
