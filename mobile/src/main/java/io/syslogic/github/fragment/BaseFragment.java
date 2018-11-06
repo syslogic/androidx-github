02fc9e799f14aebc2d12ed13b28992d68959b129
Martin Zeitler <syslogic23@gmail.com>
2018-11-06 18:04:33 +0100

    connectivity receiver (legacy) & listener interface added.

--- mobile/src/main/java/io/syslogic/github/fragment/BaseFragment.java
+++ mobile/src/main/java/io/syslogic/github/fragment/BaseFragment.java
@@ -1,27 +1,76 @@
 package io.syslogic.github.fragment;
 
 import android.content.Context;
+import android.content.IntentFilter;
 import android.net.ConnectivityManager;
+import android.net.Network;
 import android.net.NetworkInfo;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ViewFlipper;
 
 import java.util.Objects;
 
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
+
 import io.syslogic.github.BuildConfig;
+import io.syslogic.github.network.ConnectivityReceiver;
+import io.syslogic.github.network.IConnectivityListener;
 
 abstract public class BaseFragment extends Fragment {
 
+    /** {@link Log} Tag */
+    private static final String LOG_TAG = BaseFragment.class.getSimpleName();
+
     /** Debug Output */
-    protected static final boolean mDebug = BuildConfig.DEBUG;
+    static final boolean mDebug = BuildConfig.DEBUG;
+
+    /** the {@link android.widget.ViewFlipper} */
+    protected ViewFlipper mViewFlipper;
 
     public BaseFragment() {
 
     }
 
-    public static boolean isNetworkAvailable(Context context) {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    protected static ConnectivityManager getConnectivityManager(Context context) {
         ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo info = Objects.requireNonNull(cm).getActiveNetworkInfo();
-        if (info == null) {return false;}
-        else {return true;}
+        return Objects.requireNonNull(cm);
+    }
+
+    public boolean isNetworkAvailable(Context context) {
+        ConnectivityManager cm = getConnectivityManager(context);
+        NetworkInfo info = cm.getActiveNetworkInfo();
+        return info != null;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    protected void registerNetworkCallback(Context context, final IConnectivityListener listener) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {return;}
+        ConnectivityManager cm = getConnectivityManager(context);
+        cm.registerDefaultNetworkCallback(new ConnectivityManager.NetworkCallback() {
+            @Override
+            public void onLost(Network network) {
+                listener.onNetworkLost();
+            }
+            @Override
+            public void onAvailable(Network network) {
+                listener.onNetworkAvailable();
+            }
+        });
+    }
+
+    /** required < API 24 Nougat */
+    @SuppressWarnings("deprecation")
+    protected void registerBroadcastReceiver(Context context) {
+        IntentFilter intentFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        ConnectivityReceiver mReceiver = new ConnectivityReceiver(context);
+        context.registerReceiver(mReceiver, intentFilter);
     }
 }
