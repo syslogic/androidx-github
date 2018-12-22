5f2ab6047e07f5cde5d5c24927a55aab9ebf0fd4
Martin Zeitler <syslogic23@gmail.com>
2018-12-22 12:48:48 +0100

    string-array adapter added.

---
+++ mobile/src/main/java/io/syslogic/github/adapter/StringArrayAdapter.java
@@ -0,0 +1,25 @@
+package io.syslogic.github.adapter;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import io.syslogic.github.model.SpinnerItem;
+
+public class StringArrayAdapter extends BaseArrayAdapter {
+
+    ArrayList<SpinnerItem> mItems;
+
+    public StringArrayAdapter(@NonNull Context context) {
+        super(context);
+
+    }
+
+    void setItems(@NonNull Context context, String[] array) {
+        this.clearItems();
+        for(int i = 0; i < array.length; i++) {
+            this.mItems.add(i, new SpinnerItem(i + 1, array[i], array[i]));
+        }
+    }
+}
