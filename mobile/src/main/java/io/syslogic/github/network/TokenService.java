7d5958d8556c29b1c37bc3ce241d62741b5c6d7a
Martin Zeitler <syslogic23@gmail.com>
2018-12-25 08:00:09 +0100

    dummy service added.

--- mobile/src/main/java/io/syslogic/github/network/TokenService.java
+++ mobile/src/main/java/io/syslogic/github/network/TokenService.java
@@ -1,7 +1,12 @@
 package io.syslogic.github.network;
 
+import android.accounts.AbstractAccountAuthenticator;
+import android.accounts.Account;
+import android.accounts.AccountAuthenticatorResponse;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -16,21 +21,66 @@ public class TokenService extends Service {
 
     private final String LOG_TAG = TokenService.class.getSimpleName();
 
-    @Nullable
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+    private Authenticator mAuthenticator;
+
+    public static Account getAccount(String accountName, String accountType) {
+        return new Account(accountName, accountType);
     }
 
     @Override
     public void onCreate() {
-        super.onCreate();
         Log.i(LOG_TAG, "Service created");
+        mAuthenticator = new Authenticator(this);
     }
 
     @Override
     public void onDestroy() {
-        super.onDestroy();
         Log.i(LOG_TAG, "Service destroyed");
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mAuthenticator.getIBinder();
+    }
+
+    public class Authenticator extends AbstractAccountAuthenticator {
+        public Authenticator(Context context) {
+            super(context);
+        }
+
+        @Override
+        public Bundle editProperties(AccountAuthenticatorResponse accountAuthenticatorResponse, String s) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public Bundle addAccount(AccountAuthenticatorResponse accountAuthenticatorResponse, String s, String s2, String[] strings, Bundle bundle) {
+            return null;
+        }
+
+        @Override
+        public Bundle confirmCredentials(AccountAuthenticatorResponse accountAuthenticatorResponse, Account account, Bundle bundle) {
+            return null;
+        }
+
+        @Override
+        public Bundle getAuthToken(AccountAuthenticatorResponse accountAuthenticatorResponse, Account account, String s, Bundle bundle) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public String getAuthTokenLabel(String s) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public Bundle updateCredentials(AccountAuthenticatorResponse accountAuthenticatorResponse, Account account, String s, Bundle bundle) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public Bundle hasFeatures(AccountAuthenticatorResponse accountAuthenticatorResponse, Account account, String[] strings) {
+            throw new UnsupportedOperationException();
+        }
     }
 }
