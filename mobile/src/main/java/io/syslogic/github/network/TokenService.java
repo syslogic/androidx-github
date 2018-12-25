588ecb188324e71d91e86136ce4aed850090d447
Martin Zeitler <syslogic23@gmail.com>
2018-12-25 07:53:20 +0100

    dummy service added.

--- mobile/src/main/java/io/syslogic/github/network/TokenService.java
+++ mobile/src/main/java/io/syslogic/github/network/TokenService.java
@@ -3,19 +3,34 @@ package io.syslogic.github.network;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
 /**
- * TODO: Token Service
+ * Token Service
  * @author Martin Zeitler
  * @version 1.0.0
 **/
 public class TokenService extends Service {
 
+    private final String LOG_TAG = TokenService.class.getSimpleName();
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i(LOG_TAG, "Service created");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.i(LOG_TAG, "Service destroyed");
     }
 }
