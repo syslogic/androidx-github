54f2d646179c4ee6a76983ec7d6dde8597489996
Martin Zeitler <martin@syslogic.io>
2021-10-19 16:57:43 +0200

    version 1.0.9: AppTheme.SplashScreen added.

--- mobile/src/main/java/io/syslogic/github/activity/NavHostActivity.java
+++ mobile/src/main/java/io/syslogic/github/activity/NavHostActivity.java
@@ -3,11 +3,12 @@ package io.syslogic.github.activity;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.core.splashscreen.SplashScreen;
 
 import io.syslogic.github.R;
 
 /**
- * Main NavHost Activity
+ * NavHost {@link BaseActivity} using {@link SplashScreen}.
  * @author Martin Zeitler
  */
 public class NavHostActivity extends BaseActivity {
@@ -15,6 +16,7 @@ public class NavHostActivity extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SplashScreen.installSplashScreen(this);
         this.setContentView(R.layout.fragment_navhost);
     }
 }
