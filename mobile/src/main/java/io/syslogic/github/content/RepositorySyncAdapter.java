92224733584552817115f1d502fbc117435367ac
Martin Zeitler <martin@syslogic.io>
2023-08-07 07:55:05 +0200

    sync-adapter should cache the own repositories.

--- mobile/src/main/java/io/syslogic/github/content/RepositorySyncAdapter.java
+++ mobile/src/main/java/io/syslogic/github/content/RepositorySyncAdapter.java
@@ -12,20 +12,21 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.preference.PreferenceManager;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 import io.syslogic.github.BuildConfig;
 import io.syslogic.github.Constants;
 import io.syslogic.github.api.GithubClient;
-import io.syslogic.github.api.model.QueryString;
 import io.syslogic.github.api.model.Repository;
-import io.syslogic.github.api.model.RepositorySearch;
-import io.syslogic.github.api.model.User;
 import io.syslogic.github.api.room.Abstraction;
-import io.syslogic.github.api.room.QueryStringsDao;
 import io.syslogic.github.api.room.RepositoriesDao;
 import io.syslogic.github.network.TokenHelper;
+
+import okhttp3.ResponseBody;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -42,27 +43,21 @@ public class RepositorySyncAdapter extends AbstractThreadedSyncAdapter {
     @NonNull
     private static final String LOG_TAG = RepositorySyncAdapter.class.getSimpleName();
 
-    /** Debug Output */
-    static final boolean mDebug = BuildConfig.DEBUG;
-
-    final QueryStringsDao queryStringsDao;
-    final RepositoriesDao repositoriesDao;
-
-    private final String accessToken;
     private final SharedPreferences prefs;
 
-    private ArrayList<Repository> repositories = new ArrayList<>();
+    private final RepositoriesDao dao;
+
+    private final String accessToken;
+
+    private final String username;
 
     /** Constructor. */
     public RepositorySyncAdapter(@NonNull Context context, boolean autoInitialize, boolean allowParallelSyncs) {
         super(context, autoInitialize, allowParallelSyncs);
-        this.accessToken = TokenHelper.getAccessToken(context);
         this.prefs = PreferenceManager.getDefaultSharedPreferences(context);
-        this.repositoriesDao = Abstraction.getInstance(context).repositoriesDao();
-        this.queryStringsDao = Abstraction.getInstance(context).queryStringsDao();
-        if (mDebug && this.prefs.getBoolean(Constants.PREFERENCE_KEY_DEBUG_LOGGING, false)) {
-            Log.d(LOG_TAG, "Verbose logging is active.");
-        }
+        this.dao = Abstraction.getInstance(context).repositoriesDao();
+        this.accessToken = TokenHelper.getAccessToken(context);
+        this.username = TokenHelper.getUsername(context);
     }
 
     /**
@@ -77,64 +72,57 @@ public class RepositorySyncAdapter extends AbstractThreadedSyncAdapter {
      */
     @Override
     public void onPerformSync(Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) {
-        if (this.accessToken != null) {
-            Call<User> api = GithubClient.getUser(this.accessToken);
-            this.log("" + api.request().url());
-            api.enqueue(new Callback<User>() {
+        if (this.accessToken != null && this.username != null) {
+
+            int pageNumber = 1;
+            int pageSize = 100;
+
+            Call<ArrayList<Repository>> api = GithubClient.getUserRepositories(accessToken, username, "owner", "full_name", "desc", pageSize, pageNumber);
+            if (BuildConfig.DEBUG && this.prefs.getBoolean(Constants.PREFERENCE_KEY_DEBUG_LOGGING, false)) {
+                Log.d(LOG_TAG, api.request().url() + "");
+            }
+
+            api.enqueue(new Callback<>() {
                 @Override
-                public void onResponse(@NonNull Call<User> call, @NonNull Response<User> response) {
-                    if (response.body() != null) {
-                        onUser(response.body());
-                        Abstraction.executorService.execute(() -> {
-                            assert queryStringsDao != null;
-                            onQueryStrings(queryStringsDao.getItems());
-                        });
+                public void onResponse(@NonNull Call<ArrayList<Repository>> call, @NonNull Response<ArrayList<Repository>> response) {
+                    if (response.code() == 200) { // OK
+                        if (response.body() != null) {
+
+                            /* Updating the adapter with the initial response already. */
+                            ArrayList<Repository> items = response.body();
+                            for (Repository item : items) {
+                                Abstraction.executorService.execute(() -> {
+
+                                });
+                            }
+                        }
+                    } else {
+                        /* "bad credentials" means that the provided access-token is invalid. */
+                        if (response.errorBody() != null) {
+                            logError(response.errorBody());
+                        }
                     }
                 }
-                public void onFailure(@NonNull Call<User> call, @NonNull Throwable t) {
-                    if (mDebug) {Log.e(LOG_TAG, "" + t.getMessage());}
+
+                @Override
+                public void onFailure(@NonNull Call<ArrayList<Repository>> call, @NonNull Throwable t) {
+                    if (BuildConfig.DEBUG) {
+                        Log.e(LOG_TAG, "" + t.getMessage());
+                    }
                 }
             });
         }
     }
 
-    void onUser(@NonNull User item) {
-        this.log("onUser: " + item.getLogin());
-    }
-
-    void onQueryStrings(@NonNull List<QueryString> items) {
-        this.log("onQueryStrings: " + items.size());
-        for (QueryString item: items) {
-            Call<RepositorySearch> api = GithubClient.searchRepositories(this.accessToken, item.toQueryString(), "forks", "desc", 1);
-            this.log("Building content cache for query-string: " + item.toQueryString());
-            this.log("" + api.request().url());
-            api.enqueue(new Callback<RepositorySearch>() {
-                @Override
-                public void onResponse(@NonNull Call<RepositorySearch> call, @NonNull Response<RepositorySearch> response) {
-                    if (response.body() != null) {onRepositories(response.body());}
-                }
-                @Override
-                public void onFailure(@NonNull Call<RepositorySearch> call, @NonNull Throwable t) {
-                    if (mDebug) {Log.e(LOG_TAG, "" + t.getMessage());}
-                }
-            });
-        }
-    }
-
-    void onRepositories(@NonNull RepositorySearch item) {
-        this.log("onRepositories: " + item.getRepositories().size());
-        for (Repository repository: item.getRepositories()) {
-            this.log("Repo: " + repository.getUrl());
-            this.repositories.add(repository);
-        }
-        this.log("expected item count: " + item.getTotalCount());
-        this.log("expected page count: " + item.getPageCount());
-        this.log("loaded so far: " + this.repositories.size());
-    }
-
-    void log(String message) {
-        if (mDebug && this.prefs.getBoolean(Constants.PREFERENCE_KEY_DEBUG_LOGGING, false)) {
-            Log.d(LOG_TAG, message);
+    void logError(@NonNull ResponseBody responseBody) {
+        if (BuildConfig.DEBUG && this.prefs.getBoolean(Constants.PREFERENCE_KEY_DEBUG_LOGGING, false)) {
+            try {
+                String errors = responseBody.string();
+                JsonObject jsonObject = JsonParser.parseString(errors).getAsJsonObject();
+                Log.e(LOG_TAG, jsonObject.get("message").toString());
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "" + e.getMessage());
+            }
         }
     }
 }
